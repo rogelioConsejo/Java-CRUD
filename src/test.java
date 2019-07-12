84cf6d7024029d62efafaffd5f449b7e4c121003
Rogelio <rogelio.consejo@gmail.com>
2019-07-12 17:37:28 -0500

    Modificación de test.java

--- src/test.java
+++ src/test.java
@@ -7,12 +7,20 @@ public class test {
 
 
     public static void main(String[] args) {
+        String nombre = "Conejo";
+        String apellido = "Depascua";
+
         CRUD crud = new CRUD("com.mysql.jdbc.Driver",
                 "localhost:3306", "CRUD",
                 "CRUD",
                 "E_Vk75bk%%y62aHe");
 
-        crud.crearEntrada("Manuel", "Conejo");
+        if (crud.crearEntrada(nombre, apellido)) {
+            System.out.printf("Se creó una nueva entrada para %s %s\n", nombre, apellido);
+        } else {
+            System.out.printf("Error al crear entrada para %s %s\n", nombre, apellido);
+        }
+
         ResultSet resultado = crud.leerTodos();
 
 
